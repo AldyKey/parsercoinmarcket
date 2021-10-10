ae6d6e3b41f6884172a7a440b9fee46085b9f5d6
Aldiyar Akhmediyev <74849372+AldyKey@users.noreply.github.com>
2021-10-10 14:05:25 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import requests
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
 
-chrome_driver = r'C:\Users\Aldyk\Desktop\oreh\chromedriver.exe'
+chrome_driver = r'src\chromedriver.exe'
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
